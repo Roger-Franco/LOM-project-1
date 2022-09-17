da86b803646ab1cf5b3d9ae1e9fc01288ba72ab9
Roger-Franco <rogerfrancogalt@gmail.com>
2022-09-17 00:54:43 -0400

    Props e organização dos componentes #03

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,61 @@
+import './styles.css';
+import React from 'react';
+import { loadPosts } from '../../utils/load-posts';
+import { Posts } from '../../components/Posts';
+import { Button } from '../../components/Button';
+
+export class Home extends React.Component {
+  state = {
+    posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 10
+  }
+
+  async componentDidMount() {
+    await this.loadPosts()
+  }
+
+  loadPosts = async () => {
+    const { page, postsPerPage } = this.state
+    const postsAndPhotos = await loadPosts()
+
+    this.setState({
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos,
+    })
+  }
+
+  loadMorePosts = () => {
+    const {
+      posts,
+      allPosts,
+      page,
+      postsPerPage
+
+    } = this.state
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage)
+    // console.log(page, postsPerPage, nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts)
+    this.setState({ posts, page: nextPage })
+  }
+
+  // function Home() {
+  render() {
+    const { posts, page, postsPerPage, allPosts } = this.state
+    const noMorePosts = page + postsPerPage >= allPosts.length;
+    return (
+      <section className="container">
+        <Posts posts={posts} />
+        <div className="button-container">
+          <Button
+            text="Load more posts"
+            onClick={this.loadMorePosts}
+            disabled={noMorePosts}
+          />
+        </div>
+      </section>
+    );
+  }
+}
